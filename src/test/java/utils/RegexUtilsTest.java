4a3a31d899b5a63dd4a1e0b65ea4af143d77a1f9
LeeJunGyun <rainmaker0303@hanmail.net>
2020-12-30 22:06:41 +0900

    test upload

---
+++ src/test/java/utils/RegexUtilsTest.java
@@ -0,0 +1,18 @@
+package utils;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+
+public class RegexUtilsTest {
+
+    @Test
+    public void isNuberTest () throws Exception {
+        String s = "12345";
+        Assert.assertThat(true, is(RegexUtils.isNumber(s)));
+
+        s = "1234567a";
+        Assert.assertThat(false, is(RegexUtils.isNumber(s)));
+    }
+}
